87a6a20748fbbb3f3bd36bcd6d49aeb591e055ed
mehedihasanfoysal8 <mehedihasanfoysa10@gmail.com>
2026-01-01 23:28:50 +0600

    added number converstion formate

---
+++ 02_numbers/number.py
@@ -0,0 +1,53 @@
+x = 10
+y = 20
+z = 30
+
+ans = (x + y) * z
+
+# print(ans)
+
+#print(x, y, z)
+
+# any power calculate
+
+# x ** 2
+
+# my value is 4
+# 4 ** 2 
+# ans will be 16 it's call power calculate
+
+# print(x ** 50)
+
+# define octal formate 
+
+
+# here zero and o mean octal then 20 value 
+# we are already know octal base start 8 like (0 to 7)
+o = 0o20
+
+# print(o)
+
+# same as Hexadecimal base start 16 (0 to F)
+# here zero and x mean Hexadecimal then 10 value 
+
+h = 0x20
+
+# print(h)
+
+# same as binary base start 2 (0 to 1)
+# here zero and x mean Hexadecimal then 10 value 
+
+b = 0b1001
+
+# print(b)
+
+
+# building function 
+
+print(oct(20))
+print(hex(20))
+print(bin(9))
+
+# here convert in using int method just pass value and base 
+print(int('1001', 2))
+print(int('FF', 16))
